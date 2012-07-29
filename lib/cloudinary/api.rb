98774e246a5c58395b348091df570b2338e13e1b
Tal Lev-Ami <tal.levami@gmail.com>
2012-07-29 11:26:55 +0300

    Support for Cloudinary new admin API

---
+++ lib/cloudinary/api.rb
@@ -0,0 +1,132 @@
+class Cloudinary::Api
+  class Error < StandardError; end
+  class NotFound < Error; end
+  class NotAllowed < Error; end
+  class AlreadyExists < Error; end
+  class RateLimited < Error; end
+  class BadRequest < Error; end
+  class GeneralError < Error; end
+  class AuthorizationRequired < Error; end 
+  class Response < Hash
+    attr_reader :rate_limit_reset_at, :rate_limit_remaining, :rate_limit_allowed
+    def initialize(response)
+      self.update(Cloudinary::Api.send(:parse_json_response, response))
+      @rate_limit_allowed = response.headers[:x_featureratelimit_limit].to_i
+      @rate_limit_reset_at = Time.parse(response.headers[:x_featureratelimit_reset])
+      @rate_limit_remaining = response.headers[:x_featureratelimit_remaining].to_i     
+    end
+  end
+  
+  
+  def self.resource_types(options={})
+    call_api(:get, "/resources", {}, options)
+  end
+
+  def self.resources(resource_type, options={})
+    type = options[:type]
+    uri = "/resources/#{resource_type}"
+    uri += "/#{type}" if !type.blank?
+    call_api(:get, uri, only(options, :next_cursor, :max_results, :prefix), options)    
+  end
+  
+  def self.resources_by_tag(resource_type, tag, options={})
+    uri = "/resources/#{resource_type}/tags/#{tag}"
+    call_api(:get, uri, only(options, :next_cursor, :max_results), options)    
+  end
+  
+  def self.resource(resource_type, type, public_id, options={})
+    uri = "/resources/#{resource_type}/#{type}/#{public_id}"
+    call_api(:get, uri, {}, options)      
+  end
+  
+  def self.delete_resources(resource_type, type, public_ids, options={})
+    uri = "/resources/#{resource_type}/#{type}"
+    call_api(:delete, uri, {:public_ids=>public_ids}, options)      
+  end
+
+  def self.delete_resources_by_prefix(resource_type, type, prefix, options={})
+    uri = "/resources/#{resource_type}/#{type}"
+    call_api(:delete, uri, {:prefix=>prefix}, options)      
+  end
+  
+  def self.delete_derived_resources(derived_resource_ids, options={})
+    uri = "/derived_resources"
+    call_api(:delete, uri, {:derived_resource_ids=>derived_resource_ids}, options)      
+  end
+
+  def self.tags(resource_type, options={})
+    uri = "/tags/#{resource_type}"
+    call_api(:get, uri, only(options, :next_cursor, :max_results, :prefix), options)    
+  end
+  
+  def self.transformations(options={})
+    call_api(:get, "/transformations", only(options, :next_cursor, :max_results), options)    
+  end
+
+  def self.transformation(transformation, options={})
+    call_api(:get, "/transformations/#{transformation_string(transformation)}", only(options, :max_results), options)    
+  end
+  
+  def self.delete_transformation(transformation, options={})
+    call_api(:delete, "/transformations/#{transformation_string(transformation)}", {}, options)    
+  end
+  
+  # updates - currently only supported update is the "allowed_for_strict" boolean flag
+  def self.update_transformation(transformation, updates, options={})
+    call_api(:put, "/transformations/#{transformation_string(transformation)}", only(updates, :allowed_for_strict), options)
+  end
+  
+  def self.create_transformation(name, definition, options={})
+    call_api(:post, "/transformations/#{name}", {:transformation=>transformation_string(definition)}, options)
+  end
+  
+  protected
+  
+  def self.call_api(method, uri, params, options)
+    cloudinary = options[:upload_prefix] || Cloudinary.config.upload_prefix || "https://api.cloudinary.com"
+    cloud_name = options[:cloud_name] || Cloudinary.config.cloud_name || raise("Must supply cloud_name")
+    api_key = options[:api_key] || Cloudinary.config.api_key || raise("Must supply api_key")
+    api_secret = options[:api_secret] || Cloudinary.config.api_secret || raise("Must supply api_secret")
+    api_url = [cloudinary, "v1_1", cloud_name, uri].join("/")
+    # Add authentication
+    api_url.sub!(%r(^(https?://)), "\\1#{api_key}:#{api_secret}@")
+    
+    RestClient::Request.execute(:method => method, :url => api_url, :payload => params.reject{|k, v| v.nil? || v==""}, :timeout=>60) do
+      |response, request, tmpresult|
+      return Response.new(response) if response.code == 200
+      exception_class = case response.code
+        when 400 then BadRequest
+        when 401 then AuthorizationRequired
+        when 403 then NotAllowed
+        when 404 then NotFound
+        when 409 then AlreadyExists
+        when 420 then RateLimited
+        when 500 then GeneralError
+        else raise GeneralError.new("Server returned unexpected status code - #{response.code} - #{response.body}")   
+      end
+      json = parse_json_response(response)
+      raise exception_class.new(json["error"]["message"])
+    end    
+  end
+  
+  def self.parse_json_response(response)
+    return Cloudinary::Utils.json_decode(response.body)
+  rescue => e
+    # Error is parsing json
+    raise GeneralError.new("Error parsing server response (#{response.code}) - #{response.body}. Got - #{e}")
+  end
+  
+  def self.only(hash, *keys)
+    result = {}
+    keys.each do
+      |key| 
+      result[key] = hash[key] if hash.include?(key)
+      result[key] = hash[key.to_s] if hash.include?(key.to_s)
+    end
+    result
+  end  
+  
+  def self.transformation_string(transformation)
+    transformation = transformation.is_a?(String) ? transformation : Cloudinary::Utils.generate_transformation_string(transformation.clone)
+  end
+end
---
+++ spec/api_spec.rb
@@ -0,0 +1,139 @@
+require 'spec_helper'
+require 'cloudinary'
+
+describe Cloudinary::Api do
+  break puts("Please setup environment for api test to run") if Cloudinary.config.api_secret.blank?
+
+  before(:all) do
+    @api = Cloudinary::Api
+    Cloudinary::Uploader.upload("spec/logo.png", :public_id=>"api_test", :tags=>"api_test_tag", :eager=>[:width=>100,:crop=>:scale])
+    Cloudinary::Uploader.upload("spec/logo.png", :public_id=>"api_test2", :tags=>"api_test_tag", :eager=>[:width=>100,:crop=>:scale])
+    @api.delete_transformation("api_test_transformation") rescue nil
+  end
+  
+  it "should allow listing resource_types" do
+    @api.resource_types()["resource_types"].should include("image")
+  end
+
+  it "should allow listing resources" do
+    resource = @api.resources("image")["resources"].find{|resource| resource["public_id"] == "api_test"}
+    resource.should_not be_blank
+    resource["type"].should == "upload" 
+  end
+
+  it "should allow listing resources with cursor" do
+    result = @api.resources("image", :max_results=>1)
+    result["resources"].should_not be_blank
+    result["resources"].length.should == 1
+    result["next_cursor"].should_not be_blank
+    result2 = @api.resources("image", :max_results=>1, :next_cursor=>result["next_cursor"])
+    result2["resources"].should_not be_blank
+    result2["resources"].length.should == 1
+    result2["resources"][0]["public_id"].should_not == result["resources"][0]["public_id"] 
+  end
+
+
+  it "should allow listing resources by type" do
+    resource = @api.resources("image", :type=>"upload")["resources"].find{|resource| resource["public_id"] == "api_test"}
+    resource.should_not be_blank
+  end
+
+  it "should allow listing resources by prefix" do
+    public_ids = @api.resources("image", :type=>"upload", :prefix=>"api_test")["resources"].map{|resource| resource["public_id"]}
+    public_ids.should include("api_test", "api_test2")
+  end
+
+  it "should allow listing resources by tag" do
+    resource = @api.resources_by_tag("image", "api_test_tag")["resources"].find{|resource| resource["public_id"] == "api_test"}
+    resource.should_not be_blank
+  end
+
+  it "should allow get resource metadata" do
+    resource = @api.resource("image", "upload", "api_test")
+    resource.should_not be_blank
+    resource["public_id"].should == "api_test"
+    resource["bytes"].should == 3381
+    resource["derived"].length.should == 1
+  end
+  
+  it "should allow deleting derived resource" do
+    Cloudinary::Uploader.upload("spec/logo.png", :public_id=>"api_test3", :eager=>[:width=>101,:crop=>:scale])
+    resource = @api.resource("image", "upload", "api_test3")
+    resource.should_not be_blank
+    resource["derived"].length.should == 1
+    derived_resource_id = resource["derived"][0]["id"]
+    @api.delete_derived_resources(derived_resource_id)
+    resource = @api.resource("image", "upload", "api_test3")
+    resource.should_not be_blank
+    resource["derived"].length.should == 0
+  end
+
+  it "should allow deleting resources" do
+    Cloudinary::Uploader.upload("spec/logo.png", :public_id=>"api_test3")
+    resource = @api.resource("image", "upload", "api_test3")
+    resource.should_not be_blank
+    @api.delete_resources("image", "upload", ["apit_test", "api_test2", "api_test3"])
+    lambda{@api.resource("image", "upload", "api_test3")}.should raise_error(Cloudinary::Api::NotFound)
+  end
+
+  it "should allow listing tags" do
+    tags = @api.tags("image")["tags"]
+    tags.should include('api_test_tag')
+  end
+
+  it "should allow listing tag by prefix" do
+    tags = @api.tags("image", :prefix=>"api_test")["tags"]
+    tags.should include('api_test_tag')
+    tags = @api.tags("image", :prefix=>"api_test_no_such_tag")["tags"]
+    tags.should be_blank
+  end
+  
+  it "should allow listing transformations" do
+    transformation = @api.transformations()["transformations"].find{|transformation| transformation["name"] == "c_scale,w_100"}
+    transformation.should_not be_blank
+    transformation["used"].should == true
+  end
+
+  it "should allow getting transformation metadata" do
+    transformation = @api.transformation("c_scale,w_100")
+    transformation.should_not be_blank  
+    transformation["info"].should == ["crop"=>"scale", "width"=>100]     
+    transformation = @api.transformation("crop"=>"scale", "width"=>100)
+    transformation.should_not be_blank  
+    transformation["info"].should == ["crop"=>"scale", "width"=>100]     
+  end
+  
+  it "should allow updating transformation allowed_for_strict" do
+    @api.update_transformation("c_scale,w_100", :allowed_for_strict=>true)
+    transformation = @api.transformation("c_scale,w_100")
+    transformation.should_not be_blank  
+    transformation["allowed_for_strict"].should == true
+    @api.update_transformation("c_scale,w_100", :allowed_for_strict=>false)
+    transformation = @api.transformation("c_scale,w_100")
+    transformation.should_not be_blank  
+    transformation["allowed_for_strict"].should == false
+  end
+
+  it "should allow creating named transformation" do
+    @api.create_transformation("api_test_transformation", "crop"=>"scale", "width"=>102)
+    transformation = @api.transformation("api_test_transformation")
+    transformation.should_not be_blank  
+    transformation["allowed_for_strict"].should == true
+    transformation["info"].should == ["crop"=>"scale", "width"=>102]
+    transformation["used"].should == false
+  end
+
+  it "should allow deleting named transformation" do
+    @api.create_transformation("api_test_transformation2", "crop"=>"scale", "width"=>103)
+    @api.transformation("api_test_transformation2")
+    @api.delete_transformation("api_test_transformation2")
+    lambda{@api.transformation("api_test_transformation2")}.should raise_error(Cloudinary::Api::NotFound)
+  end  
+
+  it "should allow deleting implicit transformation" do
+    @api.transformation("c_scale,w_100")
+    @api.delete_transformation("c_scale,w_100")
+    lambda{@api.transformation("c_scale,w_100")}.should raise_error(Cloudinary::Api::NotFound)
+  end
+
+end
