8c4762f901fdcc4f3aa7d8cb8cdfcc2a7b10eee8
Tal Lev-Ami <tal.levami@gmail.com>
2012-09-09 11:02:25 +0300

    Allow overriding type in signed_download_url. Fix image format detection code

--- lib/cloudinary/utils.rb
+++ lib/cloudinary/utils.rb
@@ -178,7 +178,7 @@ class Cloudinary::Utils
     authenticated_distribution = options[:authenticated_distribution] || Cloudinary.config.authenticated_distribution || raise("Must supply authenticated_distribution")
     @signers ||= Hash.new{|h,k| path, id = k; h[k] = AwsCfSigner.new(path, id)}
     signer = @signers[[aws_private_key_path, aws_key_pair_id]]
-    url = Cloudinary::Utils.cloudinary_url(public_id, options.merge(:secure=>true, :secure_distribution=>authenticated_distribution, :private_cdn=>true, :type=>:authenticated))
+    url = Cloudinary::Utils.cloudinary_url(public_id, {:type=>:authenticated}.merge(options).merge(:secure=>true, :secure_distribution=>authenticated_distribution, :private_cdn=>true))
     expires_at = options[:expires_at] || (Time.now+3600)
     signer.sign(url, :ending => expires_at)
   end
@@ -232,8 +232,10 @@ class Cloudinary::Utils
     end
   end
   
+  IMAGE_FORMATS = %w(bmp png tif tiff jpg jpeg gif pdf ico) 
+  
   def self.supported_image_format?(format)
-    ['bmp', 'png', /ti?f/, /jpe?g/, 'gif', 'pdf', 'ico'].detect { |item| format =~ /(\.?)#{item}/i }
+    IMAGE_FORMATS.include?(format.to_s.downcase)
   end
   
   def self.resource_type_for_format(format)
