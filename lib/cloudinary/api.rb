4b72d54465c29dd6e77f0a9335b6e708088cd3da
Tal Lev-Ami <tal.levami@gmail.com>
2012-10-02 11:06:11 +0200

    New flags for resource information api

--- lib/cloudinary/api.rb
+++ lib/cloudinary/api.rb
@@ -40,7 +40,7 @@ class Cloudinary::Api
     resource_type = options[:resource_type] || "image"
     type = options[:type] || "upload"
     uri = "resources/#{resource_type}/#{type}/#{public_id}"
-    call_api(:get, uri, {}, options)      
+    call_api(:get, uri, only(options, :colors, :exif, :faces), options)      
   end
   
   def self.delete_resources(public_ids, options={})
