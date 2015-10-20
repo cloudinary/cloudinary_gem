1ea9c2a775579e28f1f434b296048a0ad27f3d78
Vineet Thanedar <vinthanedar@gmail.com>
2015-10-20 08:31:20 -0700

    fix class option bug in image upload tag

--- lib/cloudinary/helper.rb
+++ lib/cloudinary/helper.rb
@@ -222,7 +222,7 @@ module CloudinaryHelper
       :"data-form-data"=>cl_upload_tag_params(options),
       :"data-cloudinary-field"=>field,
       :"data-max-chunk-size"=>options[:chunk_size],
-      :"class" => [html_options[:class], "cloudinary-fileupload"].flatten.compact
+      :"class" => [html_options[:class], "cloudinary-fileupload"].flatten.compact.join(' ')
     ).reject{|k,v| v.blank?}
     content_tag("input", nil, tag_options)
   end
