d9052e71fc0a7690ce17d61d8cf969dc04f7d0b3
Amir Tocker <amir.tocker@cloudinary.com>
2017-02-14 15:09:20 +0200

    Support nested keys in CLOUDINARY_URL

--- lib/cloudinary.rb
+++ lib/cloudinary.rb
@@ -15,15 +15,15 @@ module Cloudinary
   autoload :Downloader, "cloudinary/downloader"
   autoload :Blob, "cloudinary/blob"
   autoload :PreloadedFile, "cloudinary/preloaded_file"
-  autoload :Static, "cloudinary/static"  
-  autoload :CarrierWave, "cloudinary/carrier_wave"  
-  
-  CF_SHARED_CDN = "d3jpl91pxevbkh.cloudfront.net"
-  AKAMAI_SHARED_CDN = "res.cloudinary.com"
+  autoload :Static, "cloudinary/static"
+  autoload :CarrierWave, "cloudinary/carrier_wave"
+
+  CF_SHARED_CDN         = "d3jpl91pxevbkh.cloudfront.net"
+  AKAMAI_SHARED_CDN     = "res.cloudinary.com"
   OLD_AKAMAI_SHARED_CDN = "cloudinary-a.akamaihd.net"
-  SHARED_CDN = AKAMAI_SHARED_CDN
-  
-  USER_AGENT = "CloudinaryRuby/" + VERSION
+  SHARED_CDN            = AKAMAI_SHARED_CDN
+
+  USER_AGENT      = "CloudinaryRuby/" + VERSION
   @@user_platform = ""
 
   # Add platform information to the USER_AGENT header
@@ -46,27 +46,27 @@ module Cloudinary
 
   FORMAT_ALIASES = {
     "jpeg" => "jpg",
-    "jpe" => "jpg",
-    "tif" => "tiff",
-    "ps" => "eps",
-    "ept" => "eps"
+    "jpe"  => "jpg",
+    "tif"  => "tiff",
+    "ps"   => "eps",
+    "ept"  => "eps"
   }
-  
+
   @@config = nil
-  
+
   def self.config(new_config=nil)
     first_time = @@config.nil?
-    @@config ||= OpenStruct.new((YAML.load(ERB.new(IO.read(config_dir.join("cloudinary.yml"))).result)[config_env] rescue {}))
-        
+    @@config   ||= OpenStruct.new((YAML.load(ERB.new(IO.read(config_dir.join("cloudinary.yml"))).result)[config_env] rescue {}))
+
     # Heroku support
     if first_time && ENV["CLOUDINARY_CLOUD_NAME"]
       set_config(
-        "cloud_name" => ENV["CLOUDINARY_CLOUD_NAME"],
-        "api_key" => ENV["CLOUDINARY_API_KEY"],
-        "api_secret" => ENV["CLOUDINARY_API_SECRET"],
+        "cloud_name"          => ENV["CLOUDINARY_CLOUD_NAME"],
+        "api_key"             => ENV["CLOUDINARY_API_KEY"],
+        "api_secret"          => ENV["CLOUDINARY_API_SECRET"],
         "secure_distribution" => ENV["CLOUDINARY_SECURE_DISTRIBUTION"],
-        "private_cdn" => ENV["CLOUDINARY_PRIVATE_CDN"].to_s == 'true',
-        "secure" => ENV["CLOUDINARY_SECURE"].to_s == 'true'
+        "private_cdn"         => ENV["CLOUDINARY_PRIVATE_CDN"].to_s == 'true',
+        "secure"              => ENV["CLOUDINARY_SECURE"].to_s == 'true'
       )
     elsif first_time && ENV["CLOUDINARY_URL"]
       config_from_url(ENV["CLOUDINARY_URL"])
@@ -75,26 +75,50 @@ module Cloudinary
     set_config(new_config) if new_config
     yield(@@config) if block_given?
 
-    @@config    
+    @@config
   end
-  
+
   def self.config_from_url(url)
     @@config ||= OpenStruct.new
-    uri = URI.parse(url)
+    uri      = URI.parse(url)
     set_config(
-      "cloud_name" => uri.host,
-      "api_key" => uri.user,
-      "api_secret" => uri.password,
-      "private_cdn" => !uri.path.blank?,
+      "cloud_name"          => uri.host,
+      "api_key"             => uri.user,
+      "api_secret"          => uri.password,
+      "private_cdn"         => !uri.path.blank?,
       "secure_distribution" => uri.path[1..-1]
     )
     uri.query.to_s.split("&").each do
-      |param|
+    |param|
       key, value = param.split("=")
-      set_config(key=>URI.decode(value))
-    end    
+      if isNestedKey? key
+        putNestedKey key, value
+      else
+        set_config(key => URI.decode(value))
+      end
+    end
   end
-  
+
+  def self.putNestedKey(key, value)
+    chain   = key.split(/[\[\]]+/).reject { |i| i.empty? }
+    outer   = @@config
+    lastKey = chain.pop()
+    chain.each do |innerKey|
+      inner = outer[innerKey]
+      if inner.nil?
+        inner           = OpenStruct.new
+        outer[innerKey] = inner
+      end
+      outer = inner
+    end
+    outer[lastKey] = value
+  end
+
+
+  def self.isNestedKey?(key)
+    /\w+[\[\w+\]]/ =~ key
+  end
+
   def self.app_root
     if defined? Rails::root
       # Rails 2.2 return String for Rails.root
@@ -121,10 +145,9 @@ module Cloudinary
     new_config.each{|k,v| @@config.send(:"#{k}=", v) if !v.nil?}
   end
 end
-
-# Prevent require loop if included after Rails is already initialized.
-require "cloudinary/helper" if defined?(::ActionView::Base)
-require "cloudinary/controller" if defined?(::ActionController::Base)
-require "cloudinary/railtie" if defined?(Rails) && defined?(Rails::Railtie)
-require "cloudinary/engine" if defined?(Rails) && defined?(Rails::Engine)
+  # Prevent require loop if included after Rails is already initialized.
+  require "cloudinary/helper" if defined?(::ActionView::Base)
+  require "cloudinary/controller" if defined?(::ActionController::Base)
+  require "cloudinary/railtie" if defined?(Rails) && defined?(Rails::Railtie)
+  require "cloudinary/engine" if defined?(Rails) && defined?(Rails::Engine)
 
--- spec/cloudinary_spec.rb
+++ spec/cloudinary_spec.rb
@@ -14,4 +14,19 @@ describe Cloudinary do
     expect(Cloudinary.USER_AGENT).to match( %r"Spec\/1.0 \(Test\) CloudinaryRuby/[\d.]+")
 
   end
+
+  describe 'config' do
+    before do
+      @url_backup = ENV["CLOUDINARY_URL"]
+    end
+    after do
+      ENV["CLOUDINARY_URL"] = @url_backup
+      Cloudinary::config_from_url @url_backup
+    end
+    it "should allow nested values in CLOUDINARY_URL" do
+      ENV["CLOUDINARY_URL"]  = "cloudinary://key:secret@test123?foo[bar]=value"
+      Cloudinary::config_from_url ENV["CLOUDINARY_URL"]
+      expect(Cloudinary::config.foo.bar).to eq 'value'
+    end
+  end
 end
