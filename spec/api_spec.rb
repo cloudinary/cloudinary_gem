104f9450e75553740cdc5d7e7c50fc09a56f4b66
nadavofi <nadav.ofir@gmail.com>
2017-03-16 16:25:42 +0200

    remove tag from test

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -444,7 +444,7 @@ describe Cloudinary::Api do
     end
   end
 
-  context "resource of type authenticated", :focus => true do
+  context "resource of type authenticated" do
     i = 0
     version = nil
     publicId = ""
