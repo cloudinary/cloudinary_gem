5c7300e422208039b7cd4713e9b76bed7e1167d6
CHQNguyen <xdualx@gmail.com>
2017-06-09 17:26:32 -0700

    added limit and tag then added 2 extra thumbnail image

--- samples/photo_album/app/uploaders/image_uploader.rb
+++ samples/photo_album/app/uploaders/image_uploader.rb
@@ -4,14 +4,15 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   include Cloudinary::CarrierWave
   
-  process :tags => ["photo_album_sample"]
+  process :tags => ["photo_album_sample", "special"]
   process :convert => "jpg"
+  cloudinary_transformation :transformation => {:width => 500, :height => 500, :crop => :limit}
 
   version :thumbnail do
     eager
     resize_to_fit(150, 150)
-    cloudinary_transformation :quality => 80          
-  end  
+    cloudinary_transformation :quality => 80      
+  end
 
   # For more options, see
   # http://cloudinary.com/documentation/rails_integration#carrierwave
