fc09be49c557fd699f65244b80a510fa4b5a53cf
rohan-techfreak <khanna.rohan1993@gmail.com>
2018-11-28 14:02:31 +0530

    Add namespace to the cloudinary controller to avoiding conflicts (#319)

--- lib/cloudinary.rb
+++ lib/cloudinary.rb
@@ -153,7 +153,7 @@ module Cloudinary
 end
   # Prevent require loop if included after Rails is already initialized.
   require "cloudinary/helper" if defined?(::ActionView::Base)
-  require "cloudinary/controller" if defined?(::ActionController::Base)
+  require "cloudinary/cloudinary_controller" if defined?(::ActionController::Base)
   require "cloudinary/railtie" if defined?(Rails) && defined?(Rails::Railtie)
   require "cloudinary/engine" if defined?(Rails) && defined?(Rails::Engine)
 
