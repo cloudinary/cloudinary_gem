837f35c4a3520c3e88a757fb581da75b97fb03b6
Ariel Caplan <ariel.caplan@cloudinary.com>
2019-08-27 17:42:50 +0300

    Add deleting photos from sample AS app

---
+++ samples/user_profiles_active_storage/app/controllers/photos_controller.rb
@@ -0,0 +1,21 @@
+class PhotosController < ApplicationController
+  before_action :set_user
+  before_action :set_photo, only: [:show, :destroy]
+
+  # DELETE /users/1/photos/abc
+  def destroy
+    @photo.purge
+    respond_to do |format|
+      format.html { redirect_to @user, notice: 'Photo was successfully destroyed.' }
+    end
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+
+    def set_photo
+      @photo = @user.photos.find(params[:id])
+    end
+end
--- samples/user_profiles_active_storage/config/routes.rb
+++ samples/user_profiles_active_storage/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users do
+    resources :photos, only: [:destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
