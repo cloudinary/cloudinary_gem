8c082cff7f47f4fb508908c7721003fb7cec99ef
Victor Mours <victor@ahaoho.io>
2020-03-26 15:17:23 +0100

    Update link to CarrierWave integration

--- samples/photo_album/app/uploaders/image_uploader.rb
+++ samples/photo_album/app/uploaders/image_uploader.rb
@@ -14,6 +14,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   end  
 
   # For more options, see
-  # http://cloudinary.com/documentation/rails_integration#carrierwave
+  # https://cloudinary.com/documentation/rails_carrierwave
 
 end
