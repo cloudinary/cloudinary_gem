dc1b67bdace845bd4efce2ae320db95b55b5da2c
Will Spurgin <will.spurgin@gmail.com>
2020-04-19 02:39:08 -0500

    Fix lingering Rails 6 autoload warnings (#382)
    * Move inclusion into into lazy onload hook
    
    Removes deprecation warnings (which will be outright errors in future
    releases) for Rails 6
    
    * Update lib/cloudinary/railtie.rb
    
    Co-Authored-By: Ariel Caplan <amcaplan@users.noreply.github.com>
    
    Co-authored-by: Constantine Nathanson <35217733+const-cloudinary@users.noreply.github.com>
    Co-authored-by: Ariel Caplan <amcaplan@users.noreply.github.com>

--- lib/cloudinary/railtie.rb
+++ lib/cloudinary/railtie.rb
@@ -1,8 +1,12 @@
 class Cloudinary::Railtie < Rails::Railtie
   rake_tasks do
     Dir[File.join(File.dirname(__FILE__),'../tasks/*.rake')].each { |f| load f }
-  end  
+  end
   config.after_initialize do |app|
     ActionView::Base.send :include, CloudinaryHelper
+  end
+
+  ActiveSupport.on_load(:action_controller_base) do
+    ActionController::Base.send :include, Cloudinary::CloudinaryController
   end
 end
