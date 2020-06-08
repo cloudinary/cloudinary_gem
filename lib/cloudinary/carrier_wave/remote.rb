454d2371a21acdc4f68963aeebab6f27cc81d965
Constantine Nathanson <constantine@cloudinary.com>
2020-06-08 16:05:49 +0300

    Fix `download` function in `Cloudinary::CarrierWave`
    Fixes #399

--- lib/cloudinary/carrier_wave/remote.rb
+++ lib/cloudinary/carrier_wave/remote.rb
@@ -1,10 +1,10 @@
 module Cloudinary::CarrierWave
   def download!(uri, *args)
-    return super if !self.cloudinary_should_handle_remote?
+    return super unless self.cloudinary_should_handle_remote?
     if respond_to?(:process_uri)
       uri = process_uri(uri)
     else # Backward compatibility with old CarrierWave
-      uri = URI.parse(Utils.smart_escape(Utils.smart_unescape(uri)))
+      uri = URI.parse(Cloudinary::Utils.smart_escape(Cloudinary::Utils.smart_unescape(uri)))
     end
     return if uri.to_s.blank?
     self.original_filename = @cache_id = @filename = File.basename(uri.path).gsub(/[^a-zA-Z0-9\.\-\+_]/, '')
