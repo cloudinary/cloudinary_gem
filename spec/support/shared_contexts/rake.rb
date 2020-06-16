5df528f329360886537cd7a0f1f73fc64e6d2598
Constantine Nathanson <constantine@cloudinary.com>
2020-06-16 19:00:37 +0300

    Fix `rake_spec`

--- spec/support/shared_contexts/rake.rb
+++ spec/support/shared_contexts/rake.rb
@@ -4,7 +4,7 @@ require "rake"
 shared_context "rake" do
   let(:rake)      { Rake::Application.new }
   let(:task_name) { self.class.top_level_description }
-  let(:task_path) { "lib/tasks/#{task_name.split(":").first}" }
+  let(:task_path) { "lib/tasks/#{task_name.gsub(":", "/")}" }
   subject         { rake[task_name] }
 
   def loaded_files_excluding_current_rake_file
