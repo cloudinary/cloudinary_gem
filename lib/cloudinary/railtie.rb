87a1d55fa4a17db4ae7e9042446e9927ce66e6ce
Constantine Nathanson <constantine@cloudinary.com>
2020-06-16 17:27:41 +0300

    Fix rake tasks

--- lib/cloudinary/railtie.rb
+++ lib/cloudinary/railtie.rb
@@ -1,6 +1,6 @@
 class Cloudinary::Railtie < Rails::Railtie
   rake_tasks do
-    Dir[File.join(File.dirname(__FILE__),'../tasks/*.rake')].each { |f| load f }
+    Dir[File.join(File.dirname(__FILE__),'../tasks/**/*.rake')].each { |f| load f }
   end
   config.after_initialize do |app|
     ActionView::Base.send :include, CloudinaryHelper
