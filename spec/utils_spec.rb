3f124ccb08fe195303c91421f9605528799ef155
Constantine Nathanson <constantine@cloudinary.com>
2021-10-14 17:33:21 -0700

    Fix test

--- spec/utils_spec.rb
+++ spec/utils_spec.rb
@@ -609,8 +609,8 @@ describe Cloudinary::Utils do
         ["private", { "public_id" => "logo", "type" => "private" }, "private:logo"],
         ["format", { "public_id" => "logo", "format" => "png" }, "logo.png"],
         ["video", { "resource_type" => "video", "public_id" => "cat" }, "video:cat"],
-        ["fetch", { "url" => "https://upload.wikimedia.org/wikipedia/commons/2/2b/고창갯벌.jpg" },
-         "fetch:aHR0cHM6Ly91cGxvYWQud2lraW1lZGlhLm9yZy93aWtpcGVkaWEvY29tbW9ucy8yLzJiL-qzoOywveqwr-uyjC5qcGc="],
+        ["fetch", { "url" => "https://res.cloudinary.com/demologos/cloudinary_full_logo_white_small.png" },
+         "fetch:aHR0cHM6Ly9yZXMuY2xvdWRpbmFyeS5jb20vZGVtb2xvZ29zL2Nsb3VkaW5hcnlfZnVsbF9sb2dvX3doaXRlX3NtYWxsLnBuZw=="],
       ] }
       it "should support #{param}" do
         layers_options.each do |name, options, result|
