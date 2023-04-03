a5f8d2a67c525bc9574d4a25c1cee5e05d558324
Constantine Nathanson <constantine@cloudinary.com>
2023-04-03 17:33:49 +0300

    Add support for large files in active storage
    Fixes #510

--- spec/active_storage/database/setup.rb
+++ spec/active_storage/database/setup.rb
@@ -2,6 +2,11 @@
 
 require_relative "create_users_migration"
 
+module YAML
+  class << self
+    alias_method :load, :unsafe_load if YAML.respond_to? :unsafe_load
+  end
+end
 ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ":memory:")
 ActiveRecord::Base.connection.migration_context.migrate
 ActiveStorageCreateUsers.migrate(:up)
