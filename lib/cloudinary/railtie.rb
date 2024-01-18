2bd2e02d9d3e22f4f63f77fae2e918ec5be5ef6c
maximerety <maxime.rety@gmail.com>
2024-01-18 14:25:22 +0100

    Avoid early loading of ActionView::Base
    The injection of the CloudinaryHelper module can happen lazily when
    ActionView::Base is loaded by some other code needing it, and there is
    no reason to trigger the loading early in the railtie.
    
    A consequence of this change is that you can avoid the loading of
    ActionView::Base altogether in some cases, e.g. if running a specific
    test that does not need it, or when opening a rails console.

--- lib/cloudinary/railtie.rb
+++ lib/cloudinary/railtie.rb
@@ -3,7 +3,9 @@ class Cloudinary::Railtie < Rails::Railtie
     Dir[File.join(File.dirname(__FILE__),'../tasks/**/*.rake')].each { |f| load f }
   end
   config.after_initialize do |app|
-    ActionView::Base.send :include, CloudinaryHelper
+    ActiveSupport.on_load(:action_view) do
+      ActionView::Base.send :include, CloudinaryHelper
+    end
   end
 
   ActiveSupport.on_load(:action_controller_base) do
