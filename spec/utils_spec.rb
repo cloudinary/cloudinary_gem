08c973401e98b69fd9cef7a1af3469421f07fb13
Constantine Nathanson <constantine@cloudinary.com>
2025-06-16 16:28:45 +0300

    Fix `Cloudinary::Utils.verify_api_response_signature` backwards compatibility

--- spec/utils_spec.rb
+++ spec/utils_spec.rb
@@ -995,36 +995,37 @@ describe Cloudinary::Utils do
       .to raise_error("Unsupported algorithm 'unsupported_algorithm'")
   end
 
-  it "should prevent parameter smuggling via & characters in parameter values" do
-    # Test with notification_url containing & characters
+  it "should prevent parameter smuggling via & characters in parameter values with signature version 2" do
     params_with_ampersand = {
       :cloud_name       => API_SIGN_REQUEST_CLOUD_NAME,
       :timestamp        => 1568810420,
       :notification_url => "https://fake.com/callback?a=1&tags=hello,world"
     }
 
-    signature_with_ampersand = Cloudinary::Utils.api_sign_request(params_with_ampersand, API_SIGN_REQUEST_TEST_SECRET)
+    signature_v1_with_ampersand = Cloudinary::Utils.api_sign_request(params_with_ampersand, API_SIGN_REQUEST_TEST_SECRET, nil, 1)
+    signature_v2_with_ampersand = Cloudinary::Utils.api_sign_request(params_with_ampersand, API_SIGN_REQUEST_TEST_SECRET, nil, 2)
 
-    # Test that attempting to smuggle parameters by splitting the notification_url fails
     params_smuggled = {
       :cloud_name       => API_SIGN_REQUEST_CLOUD_NAME,
       :timestamp        => 1568810420,
       :notification_url => "https://fake.com/callback?a=1",
-      :tags             => "hello,world" # This would be smuggled if & encoding didn't work
+      :tags             => "hello,world"
     }
 
-    signature_smuggled = Cloudinary::Utils.api_sign_request(params_smuggled, API_SIGN_REQUEST_TEST_SECRET)
+    signature_v1_smuggled = Cloudinary::Utils.api_sign_request(params_smuggled, API_SIGN_REQUEST_TEST_SECRET, nil, 1)
+    signature_v2_smuggled = Cloudinary::Utils.api_sign_request(params_smuggled, API_SIGN_REQUEST_TEST_SECRET, nil, 2)
 
-    # The signatures should be different, proving that parameter smuggling is prevented
-    expect(signature_with_ampersand).not_to eq(signature_smuggled)
+    # Version 1 is vulnerable to parameter smuggling
+    expect(signature_v1_with_ampersand).to eq(signature_v1_smuggled)
 
-    # Verify the expected signature for the properly encoded case
-    expected_signature = "4fdf465dd89451cc1ed8ec5b3e314e8a51695704"
-    expect(signature_with_ampersand).to eq(expected_signature)
+    # Version 2 prevents parameter smuggling
+    expect(signature_v2_with_ampersand).not_to eq(signature_v2_smuggled)
 
-    # Verify the expected signature for the smuggled parameters case
-    expected_smuggled_signature = "7b4e3a539ff1fa6e6700c41b3a2ee77586a025f9"
-    expect(signature_smuggled).to eq(expected_smuggled_signature)
+    expected_v2_signature = "4fdf465dd89451cc1ed8ec5b3e314e8a51695704"
+    expect(signature_v2_with_ampersand).to eq(expected_v2_signature)
+
+    expected_v2_smuggled_signature = "7b4e3a539ff1fa6e6700c41b3a2ee77586a025f9"
+    expect(signature_v2_smuggled).to eq(expected_v2_smuggled_signature)
   end
 
   describe ":if" do
@@ -1317,6 +1318,43 @@ describe Cloudinary::Utils do
           api_response_signature_sha256,
           Cloudinary::Utils::ALGO_SHA256)
       ).to be true
+    end
+
+    it "should use signature version 1 (without parameter encoding) for backward compatibility" do
+      public_id_with_ampersand = 'tests/logo&version=2'
+      
+      expected_signature_v1 = Cloudinary::Utils.api_sign_request(
+        { :public_id => public_id_with_ampersand, :version => test_version },
+        SIGNATURE_VERIFICATION_API_SECRET,
+        nil,
+        1
+      )
+      
+      expected_signature_v2 = Cloudinary::Utils.api_sign_request(
+        { :public_id => public_id_with_ampersand, :version => test_version },
+        SIGNATURE_VERIFICATION_API_SECRET,
+        nil,
+        2
+      )
+      
+      expect(expected_signature_v1).not_to eq(expected_signature_v2)
+      
+      # verify_api_response_signature should use version 1 for backward compatibility
+      expect(
+        Cloudinary::Utils.verify_api_response_signature(
+          public_id_with_ampersand,
+          test_version,
+          expected_signature_v1
+        )
+      ).to be true
+      
+      expect(
+        Cloudinary::Utils.verify_api_response_signature(
+          public_id_with_ampersand,
+          test_version,
+          expected_signature_v2
+        )
+      ).to be false
     end
   end
 
